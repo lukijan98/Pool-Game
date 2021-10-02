4eb62ef55e0886bb0b43d66f881d101cf7414137
Luka <jelicl@ymail.com>
2021-10-02 20:42:40 +0200

    almost there

--- pool-game-web/Controllers/ReservationsController.cs
+++ pool-game-web/Controllers/ReservationsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using pool_game_web.Data;
@@ -28,16 +29,24 @@ namespace pool_game_web.Controllers
         }
 
         // GET: Reservations
+        [Authorize(Roles="Admin")]
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Reservations.Include(r => r.IdentityUser).Include(r => r.PoolTable);
+            var applicationDbContext = _context.Reservations.Include(r => r.IdentityUser).Include(r => r.PoolTable).AsNoTracking();
             return View(await applicationDbContext.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult GetReservationsForUser(){
+            var user =  _context.IdentityUsers.Where(t => t.Email == User.FindFirstValue(ClaimTypes.Email)).ToList();
+            var res = _context.Reservations.Include(r => r.IdentityUser).Include(r => r.PoolTable).Where(r=>r.IdentityUserId==user[0].Id).AsNoTracking().ToList();
+            return View(res);
         }
 
 
         [HttpGet]
         public IActionResult GetReservations(){
-            var res = _context.Reservations.Include(r => r.IdentityUser).Include(r => r.PoolTable).ToList();
+            var res = _context.Reservations.Include(r => r.IdentityUser).Include(r => r.PoolTable).AsNoTracking().ToList();
             return Ok(res);
         }
 
@@ -88,11 +97,8 @@ namespace pool_game_web.Controllers
                                 .ToList();
                    // .FirstOrDefault();
                 IList<PoolTable> tables = _context.PoolTables.ToList();
-                Console.WriteLine(tables.Count);
-                for(int i=0;i<tables.Count;i++)
-                {
-                    Console.WriteLine(tables[i]);
-                }       
+                if(tables.Count!=0)
+                {    
                 if(res.Count == 0||res==null){
                     reservation.PoolTableId = tables[0].PoolTableId;
                     _context.Add(reservation);
@@ -103,7 +109,6 @@ namespace pool_game_web.Controllers
                 else{
                     foreach (Reservation reserv in res)
                     {
-                        Console.WriteLine(reserv.PoolTableId);
                         PoolTable itemToRemove = tables.SingleOrDefault(r => r.PoolTableId == reserv.PoolTableId);
                         tables.Remove(itemToRemove);
                     }
@@ -114,8 +119,56 @@ namespace pool_game_web.Controllers
                         await _signalrHub.Clients.All.SendAsync("LoadReservations");
                         return RedirectToAction(nameof(Index));
                     }      
+                }
                 }    
             }
+            
+            ModelState.AddModelError("", "No available table at that time and date");
+            var a = await _context.IdentityUsers.Where(t => t.Email == User.FindFirstValue(ClaimTypes.Email)).ToListAsync();
+            ViewData["IdentityUserId"] = new SelectList(a,"Id","Email");
+           // ViewData["IdentityUserId"] = new SelectList(_context.IdentityUsers, "Id", "Id");
+            ViewData["PoolTableId"] = new SelectList(_context.PoolTables, "PoolTableId", "PoolTableId");
+            return View(reservation);
+        }
+        [HttpPost, ActionName("CreateForUser")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateForUser([Bind("ReservationId,ReservationName,Date,TimeStart,TimeEnding,IdentityUserId,PoolTableId")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine("Usao 1");
+                IList<Reservation> res = _context.Reservations
+                    .Where(r => (r.Date == reservation.Date) && (((TimeSpan.Compare(reservation.TimeStart,r.TimeStart)<=0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeEnding)>=0))||
+                                ((TimeSpan.Compare(reservation.TimeStart,r.TimeStart)>=0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeEnding)<=0))||
+                                ((TimeSpan.Compare(reservation.TimeStart,r.TimeEnding)<0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeEnding)>0))||
+                                ((TimeSpan.Compare(reservation.TimeStart,r.TimeStart)<0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeStart)>0))) )
+                                .ToList();
+                   // .FirstOrDefault();
+                IList<PoolTable> tables = _context.PoolTables.ToList();     
+                if(res.Count == 0||res==null){
+                    reservation.PoolTableId = tables[0].PoolTableId;
+                    _context.Add(reservation);
+                    await _context.SaveChangesAsync();
+                    await _signalrHub.Clients.All.SendAsync("LoadReservations");
+                    return RedirectToAction(nameof(GetReservationsForUser));
+                }       
+                else{
+                    foreach (Reservation reserv in res)
+                    {
+
+                        PoolTable itemToRemove = tables.SingleOrDefault(r => r.PoolTableId == reserv.PoolTableId);
+                        tables.Remove(itemToRemove);
+                    }
+                    if(tables.Count != 0){
+                        reservation.PoolTableId = tables[0].PoolTableId;
+                        _context.Add(reservation);
+                        await _context.SaveChangesAsync();
+                        await _signalrHub.Clients.All.SendAsync("LoadReservations");
+                        return RedirectToAction(nameof(GetReservationsForUser));
+                    }      
+                }    
+            }
+            
             ModelState.AddModelError("", "No available table at that time and date");
             var a = await _context.IdentityUsers.Where(t => t.Email == User.FindFirstValue(ClaimTypes.Email)).ToListAsync();
             ViewData["IdentityUserId"] = new SelectList(a,"Id","Email");
@@ -156,6 +209,79 @@ namespace pool_game_web.Controllers
 
             if (ModelState.IsValid)
             {
+                Reservation old = _context.Reservations.Find(reservationId);
+                bool eligableToUpdate = false;
+                if(!((TimeSpan.Compare(reservation.TimeStart,old.TimeStart)==0)&&(TimeSpan.Compare(reservation.TimeEnding,old.TimeEnding)==0)&&(DateTime.Compare(reservation.Date,old.Date)==0)))
+                {
+                    IList<Reservation> res = _context.Reservations
+                    .Where(r => (r.Date == reservation.Date) && (((TimeSpan.Compare(reservation.TimeStart,r.TimeStart)<=0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeEnding)>=0))||
+                                ((TimeSpan.Compare(reservation.TimeStart,r.TimeStart)>=0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeEnding)<=0))||
+                                ((TimeSpan.Compare(reservation.TimeStart,r.TimeEnding)<0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeEnding)>0))||
+                                ((TimeSpan.Compare(reservation.TimeStart,r.TimeStart)<0)&&(TimeSpan.Compare(reservation.TimeEnding,r.TimeStart)>0))) )
+                                .AsNoTracking()
+                                .ToList();
+                    if((res.Count == 0)||((res.Count == 1)&&(res[0].ReservationId==reservationId))){
+                        eligableToUpdate = true;
+                    }
+                    else{
+                        IList<PoolTable> tables = _context.PoolTables.ToList();
+                        foreach (Reservation reserv in res)
+                        {       
+                            PoolTable itemToRemove = tables.SingleOrDefault(r => r.PoolTableId == reserv.PoolTableId);
+                            tables.Remove(itemToRemove);
+                        }
+                        if(tables.Count != 0){
+                            reservation.PoolTableId = tables[0].PoolTableId;
+                            eligableToUpdate = true;
+                        }
+                    }
+                }
+                else
+                {
+                    eligableToUpdate = true;
+                }
+                _context.Entry(old).State = EntityState.Detached;
+                if(eligableToUpdate){
+                    try
+                    {
+
+                        _context.Update(reservation);
+                        await _context.SaveChangesAsync();
+                        await _signalrHub.Clients.All.SendAsync("LoadReservations");
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!ReservationExists(reservation.ReservationId))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+            ModelState.AddModelError("", "No available table at that time and date");
+            var a = await _context.IdentityUsers.Where(t => t.Email == User.FindFirstValue(ClaimTypes.Email)).ToListAsync();
+            ViewData["IdentityUserId"] = new SelectList(a,"Id","Email");
+           // ViewData["IdentityUserId"] = new SelectList(_context.IdentityUsers, "Id", "Id", reservation.IdentityUserId);
+            ViewData["PoolTableId"] = new SelectList(_context.PoolTables, "PoolTableId", "PoolTableId", reservation.PoolTableId);
+            return View(reservation);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditForUser(int reservationId, [Bind("ReservationId,ReservationName,Date,TimeStart,TimeEnding,IdentityUserId,PoolTableId")] Reservation reservation)
+        {
+            if (reservationId != reservation.ReservationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
                 try
                 {
                     _context.Update(reservation);
@@ -173,7 +299,7 @@ namespace pool_game_web.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(GetReservationsForUser));
             }
             ViewData["IdentityUserId"] = new SelectList(_context.IdentityUsers, "Id", "Id", reservation.IdentityUserId);
             ViewData["PoolTableId"] = new SelectList(_context.PoolTables, "PoolTableId", "PoolTableId", reservation.PoolTableId);
@@ -210,6 +336,17 @@ namespace pool_game_web.Controllers
             await _context.SaveChangesAsync();
             await _signalrHub.Clients.All.SendAsync("LoadReservations");
             return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost,ActionName("DeleteConfirmedForUser")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmedForUser(int reservationId)
+        {
+            var reservation = await _context.Reservations.FindAsync(reservationId);
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+            await _signalrHub.Clients.All.SendAsync("LoadReservations");
+            return RedirectToAction(nameof(GetReservationsForUser));
         }
 
         private bool ReservationExists(int id)
--- pool-game-web/wwwroot/js/site.js
+++ pool-game-web/wwwroot/js/site.js
@@ -18,31 +18,11 @@ $(()=>{
         $.ajax({
             url:'/Reservations/GetReservations',
             method: 'GET',
-            //dataType : "html",
             success:(result)=>{
-                console.log(result);
-                //console.log($(result).find('#table'));
-                // var wrapper=document.createElement('div');
-                // wrapper.innerHTML=result;
-                 //var t = result.getElementById("table");
-                // var tr = t.getElementsByTagName("tr");
-                // for (let i = 1; i < tr.length; i++) {
-                //     var td = tr[i].getElementsByTagName("td");
-                //     final+=`<tr>
-                //                 `;
-                //     for (let j = 0; j < td.length;j++){
-                //         final+=`<td>`+td[j].innerText+`</td>
-                //                 `;
-                //     }
-                //     final+='</tr>';
-                //   } 
-                //   console.log(final);
-                // console.log(tr.getElementsByTagName("td")[0].innerText);
-                // console.log(tr.getElementsByTagName("td")[1].innerText);
                 $.each(result,(k,v)=>{
                     tr+=`<tr>
                         <td>${v.reservationName}</td>
-                        <td>${v.date}</td>
+                        <td>${v.date.split("T00:00:00")[0]}</td>
                         <td>${v.timeStart}</td>
                         <td>${v.timeEnding}</td>
                         <td>${v.identityUser.email}</td>
